496f3baf6e1dec655c74271ca97f92620b063efc
lihui <1075985184@qq.com>
2021-06-20 21:04:15 +0800

    111

--- duoxiancheng/src/main/java/duo/xian/cheng/demo/com/leguansuo/test1.java
+++ duoxiancheng/src/main/java/duo/xian/cheng/demo/com/leguansuo/test1.java
@@ -11,6 +11,9 @@ package duo.xian.cheng.demo.com.leguansuo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 
 public class test1 {
 
@@ -19,7 +22,7 @@ public class test1 {
     public static void main(String[] args) {
 
 
-
+        ArrayList<String> objects = new ArrayList<>();
 
 
     }
